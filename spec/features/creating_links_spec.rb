e2c86f8c4bc3eb76572235efb6522cdaeee08a08
Adam <adamjbrowner@gmail.com>
2016-07-26 15:48:40 +0100

    Challenge 11

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,13 +1,12 @@
 require 'capybara/rspec'
 require './app/models/link'
-
 require './app/app'
 
 feature 'Creating links' do
 
   scenario 'save a new Link' do
     visit '/links/new_link'
-    fill_in 'title', with: 'bbc'
+    fill_in 'title', with: 'BBC'
     fill_in 'url', with: 'www.bbc.com'
     click_button 'Submit'
 
