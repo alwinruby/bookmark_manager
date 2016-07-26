f15284fcaa0a981c44ddd422d82ed28f288e23c7
alwinsolanky <alwinsolanky@gmail.com>
2016-07-26 15:26:49 +0100

    Step 11 almost there

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,21 @@
+require 'capybara/rspec'
+require './app/models/link'
+
+require './app/app'
+
+feature 'Creating links' do
+
+  scenario 'save a new Link' do
+    visit '/links/new_link'
+    fill_in 'title', with: 'bbc'
+    fill_in 'url', with: 'www.bbc.com'
+    click_button 'Submit'
+
+    # we expect to be redirected back to the links page
+    expect(current_path).to eq '/links'
+
+    within 'ul#links' do
+      expect(page).to have_content "BBC"
+    end
+  end
+end
--- spec/features/view_links_homepage_spec.rb
+++ spec/features/view_links_homepage_spec.rb
@@ -4,14 +4,16 @@ require './app/models/link'
 feature 'See list of links' do
 
   scenario 'on homepage' do
-    Link.create(url: 'http://www.google.com', title: 'Google')
+    #Link.create(url: 'http://www.google.com', title: 'Google')
     # Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    Link.create(url: 'https://www.ebay.co.uk', title: 'ebay')
     visit '/links'
 
     expect(page.status_code).to eq 200
 
     within 'ul#links' do
-      expect(page).to have_content "Google"
+      # expect(page).to have_content "Google"
+      expect(page).to have_content "ebay"
     end
   end
 end
