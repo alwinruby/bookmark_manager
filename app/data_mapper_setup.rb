11933a6ecc362097bb75da903aa122f391ca40f1
benjamin-white <benjaminwhite@live.co.uk>
2016-07-27 19:22:56 +0100

    update Gemfile

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -6,4 +6,6 @@ require_relative 'models/link'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
+# DataMapper.auto_upgrade!
+
+# Removed Step 17, as functionality now in Rakefile
